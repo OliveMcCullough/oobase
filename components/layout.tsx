d29304b7fce9b2c7f208f5d2c89d194e29392165
Olive McCullough <olive.m.mccullough@gmail.com>
2022-12-19 18:23:00 +0000

    Preload fonts in the layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,6 +7,20 @@ export default function Layout({ children }: { children:React.ReactNode }) {
             <title>Oobase</title>
             <meta name="description" content="A site to quickly find information about specific ooblets" />
             <link rel="icon" href="/favicon.ico" />
+            <link
+                rel="preload"
+                href="/fonts/FranxurterTotallyMedium.woff2"
+                as="font"
+                crossOrigin=""
+                type="font/woff2"
+            />
+            <link
+                rel="preload"
+                href="/fonts/FranxurterTotallyFat.woff2"
+                as="font"
+                crossOrigin=""
+                type="font/woff2"
+            />
         </Head>
 
         <main>
