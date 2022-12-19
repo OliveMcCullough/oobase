dcf2ec9676facfea5a5230e081ee7369f8ff1a35
Olive McCullough <olive.m.mccullough@gmail.com>
2022-12-19 10:52:29 +0000

    Made changes to returned ooblet info on individual page

--- pages/api/ooblet/by_name/[name].tsx
+++ pages/api/ooblet/by_name/[name].tsx
@@ -6,8 +6,38 @@ const prisma = new PrismaClient()
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     const oobletName = req.query.name
     if (typeof oobletName !== "string") {
-        return res.status(400).json({ data: 'Item amount has not been provided correctly'})
+        return res.status(400).json({ data: 'Error: no name found'})
     }
-    const oobletInfo = await prisma.ooblet.findFirstOrThrow({where: {name:oobletName}});
+    const oobletInfo = await prisma.ooblet.findFirst(
+        {where: 
+            {name: oobletName },
+            select: {
+                name: true,
+                desc: true,
+                moves: {
+                    select: {
+                        name: true,
+                        desc: true,
+                        cost: true
+                    }
+                }, 
+                regions: {
+                    select: {
+                        name: true,
+                    }
+                }, 
+                itemType: {
+                    select: {
+                        name: true,
+                    }
+                },
+                itemAmount: true
+            }
+        }
+    );
+    if (oobletInfo === null) {
+        return res.status(400).json({ data: 'No ooblets were found with this name'})
+    }
+    
     res.status(200).json(oobletInfo)
 }
