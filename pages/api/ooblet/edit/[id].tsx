a3896d9f8f830e555aa82469f21bf0e00cf921a0
Olive McCullough <olive.m.mccullough@gmail.com>
2022-12-19 21:59:09 +0000

    Made edit form and API handler to edit Ooblets

---
+++ pages/api/ooblet/edit/[id].tsx
@@ -0,0 +1,110 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient()
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    if (process.env.NODE_ENV != "production") {
+        const { id } = req.query
+        const body = req.body // get request data body
+
+        if (!id) {
+            return res.status(400).json({ data: 'Could not find ID' })
+        }
+
+        if(!(typeof id === "string")) {
+            return res.status(400).json({ data: 'PID is not a string' })
+        }
+
+        if(isNaN(parseInt(id))) {
+            return res.status(400).json({ data: 'PID is not a number' })
+        }
+
+        // verify data is correct
+        // all fields are present
+        if (!body.name || !body.description || !body.regions || !body.itemType || !body.itemAmount || 
+            !body.moveName0 || !body.moveName1 || !body.moveName2 ||
+            !body.moveDescription0 || !body.moveDescription1 || !body.moveDescription2 ||
+            !body.moveCost0 || !body.moveCost1 || !body.moveCost2) {
+            return res.status(400).json({ data: 'Full data for Ooblet not present' })
+        }
+        // item amount is a number
+        if(!!isNaN(body.itemAmount)) {
+            return res.status(400).json({ data: 'Item amount has not been provided correctly'})
+        }
+        // item amount is above 3 or under 1
+        if(parseFloat(body.itemAmount) < 1 || parseFloat(body.itemAmount) > 3) {
+            return res.status(400).json({ data: 'Item amount must be between 1 and 3'})
+        }
+        // there is not at least one region
+        if(body.regions.length === 0 && typeof body.regions !== "string") {
+            return res.status(400).json({ data: 'There must be at least 1 region'})
+        }
+        // the move costs are numbers
+        if(!!isNaN(body.moveCost0) || !!isNaN(body.moveCost1) || !!isNaN(body.moveCost2)) {
+            return res.status(400).json({ data: 'Move costs should be numerical'})
+        }
+        // the move costs are 0 or higher
+        if(parseFloat(body.moveCost0) < 0 || parseFloat(body.moveCost1) < 0 || parseFloat(body.moveCost2) < 0) {
+            return res.status(400).json({ data: 'Move costs should be positive'})
+        }
+        // the ooblet doesn't already exist
+        if(await prisma.ooblet.findFirst( { where: {name:body.name, NOT: {id:parseInt(id)}} } ) !== null) {
+            return res.status(400).json({ data: 'There is already an Ooblet with that name!'})
+        }
+
+        // prepare array of new regions
+        let newRegions:string[]
+        if(typeof body.regions === "string") {
+            newRegions = [body.regions]
+        } else {
+            newRegions = body.regions
+        }
+
+        // remove all associated moves
+        await prisma.move.deleteMany({where: {ooblet:{ id: parseInt(id) }}})
+
+        // update ooblet
+        const ooblet = await prisma.ooblet.update({
+            where: { id: parseInt(id) },
+            data: {
+                name: body.name,
+                desc: body.description,
+                itemAmount: parseInt(body.itemAmount),
+                regions: { connectOrCreate: newRegions.map((region:string) => {
+                    return {where: { name: region },
+                    create: { name: region }}
+                }
+                ) },
+                itemType: { update: {name: body.itemType} },
+                moves: {
+                    create: [
+                      {
+                        name: body.moveName0, 
+                        desc: body.moveDescription0,
+                        cost: parseInt(body.moveCost0),
+                        level: 1,
+                      },
+                      {
+                        name: body.moveName1, 
+                        desc: body.moveDescription1,
+                        cost: parseInt(body.moveCost1),
+                        level: 3,
+                      },
+                      {
+                        name: body.moveName2, 
+                        desc: body.moveDescription2,
+                        cost: parseInt(body.moveCost2),
+                        level: 6,
+                      }
+                    ]
+                }
+            },
+        })
+
+
+        // return something to confirm action has been performed
+        res.redirect("/ooblet/edit/" + id)
+    }
+}
+  
---
+++ pages/ooblet/edit/[id].tsx
@@ -0,0 +1,148 @@
+import Creatable, { useCreatable } from 'react-select/creatable';
+import { GetServerSideProps } from "next";
+import { PrismaClient } from "@prisma/client";
+import Layout from '../../../components/layout';
+
+
+type OptionType = {
+    value: string;
+    label: string;
+}
+
+type OobletData = {
+    id: number;
+    name: string;
+    desc: string;
+    itemAmount: number;
+    itemType: {
+        id: number;
+        name: string;
+    },
+    regions: [{
+        id: number;
+        name: string;
+    }],
+    moves: [{
+        id: number;
+        name: string;
+        desc: string;
+        cost: number;
+        level: number;
+    }]
+}
+
+export const getServerSideProps:GetServerSideProps = async ({ params }) => {
+
+    const redirect = {
+        redirect: {
+            permanent: false,
+            destination: "/",
+        },
+    }
+    if (process.env.NODE_ENV === "production") {
+        return redirect;
+    }
+
+    const prisma = new PrismaClient()
+    const regions = await prisma.region.findMany()
+    const regionOptions:OptionType[] = regions.map((region) => ({value: region.name +"", label: region.name}))
+
+    if(typeof params?.id === "string") {
+        if(!isNaN(parseInt(params?.id))) {
+            const oobletData = await prisma.ooblet.findFirst(
+                {where: {id:parseInt(params?.id)}, 
+                    select: {
+                        name: true,
+                        desc: true,
+                        regions: true,
+                        itemType: true,
+                        moves: {
+                            orderBy: {
+                                level: 'asc'
+                            }
+                        },
+                        itemAmount: true
+                    }
+                }
+            )
+
+            const oobletID=params?.id
+
+            return {
+                props : { 
+                    regionOptions,
+                    oobletData,
+                    oobletID
+                }
+            }
+        }
+    }
+
+    return redirect;
+}
+
+export default function EditOobletPage ({regionOptions, oobletData, oobletID} : {regionOptions:OptionType[], oobletData:OobletData, oobletID:number}) {
+    return (
+        <Layout> 
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-4 mb-4 m-auto w-4/5">
+                <h1 className="text-3xl font-bold text-center"> Edit {oobletData.name} </h1>
+                <form action={"/api/ooblet/edit/" + oobletID} method="post">
+                    <label htmlFor="name" className="form-label"> Name: </label>
+                    <input type="text" id="name" name="name" className="form-input" defaultValue={oobletData.name} required/>
+
+                    <label htmlFor="description" className="form-label"> Description: </label>
+                    <textarea id="description" name="description" className="form-input"  defaultValue={oobletData.desc} required/>
+
+                    <label htmlFor="regions" className="form-label"> Region: </label>
+                    <Creatable 
+                        required
+                        inputId="regions" 
+                        name="regions" 
+                        options={regionOptions} 
+                        isMulti={true}
+                        classNames={{
+                            control: (state) =>
+                                `form-input ${state.isFocused?'form-input-focus':''}`,
+                            menu: (state) =>
+                                'react-select-menu',
+                            option: (state) =>
+                                `react-select-option ${state.isFocused? 'react-select-option-selected':''}`,
+                            noOptionsMessage: (state) =>
+                                'react-select-no-option',
+                            multiValue: (state) =>
+                                'react-select-value',
+                            multiValueLabel: (state) =>
+                                'react-select-value-label',
+                            multiValueRemove: (state) =>
+                                'react-select-value-remove',
+                        }}
+                        unstyled={true}
+                        defaultValue={oobletData.regions.map((region) => ({value: region.name, label: region.name}))}
+                    />
+
+                    <label htmlFor="itemType" className="form-label"> Item type: </label>
+                    <input type="text" id="itemType" name="itemType" className="form-input" defaultValue={oobletData.itemType.name} required/>
+
+                    <label htmlFor="itemAmount" className="form-label"> Item amount: </label> 
+                    <input type="number" id="itemAmount" name="itemAmount" className="form-input" min="1" max="4" defaultValue={oobletData.itemAmount} required/>
+
+                    {[...Array(3)].map((x, i) =>
+                        <fieldset key={i} className="form-fieldset"> 
+                            <legend className="form-fieldset-legend"> Move n°{i+1} (level {(i+1)/2*(i+2)})</legend>
+                            <label htmlFor={`moveName${i}`} className="form-label">Name:</label>
+                            <input type="text" id={`moveName${i}`} name={`moveName${i}`} className="form-input" defaultValue={(oobletData.moves[i]?oobletData.moves[i].name:"")} required/>
+
+                            <label htmlFor={`moveDescription${i}`} className="form-label">Description:</label>
+                            <textarea id={`moveDescription${i}`} name={`moveDescription${i}`} className="form-input" defaultValue={(oobletData.moves[i]?oobletData.moves[i].desc:"")} required/>
+
+                            <label htmlFor={`moveCost${i}`} className="form-label">Cost:</label>
+                            <input type="number" min="0" id={`moveCost${i}`} name={`moveCost${i}`} className="form-input" defaultValue={(oobletData.moves[i]?oobletData.moves[i].cost:"")} required/>
+                        </fieldset>
+                    )}
+
+                    <button type="submit" className="form-button"> I Know It </button>
+                </form>
+            </div>
+        </Layout>
+    )
+}
