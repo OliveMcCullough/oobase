edf95d3cd41a7595c90ee4d2b7895f33e247ae34
Olive McCullough <olive.m.mccullough@gmail.com>
2023-01-10 00:19:10 +0000

    Added some additional animations into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,25 @@ module.exports = {
         seesaw: {
           '0%, 100%': {transform: 'rotate(5deg)'},
           '50%': {transform: 'rotate(-5deg)'}
+        },
+        twinkle: {
+          '0%, 100%': {opacity: 0},
+          '50%': {opacity: 1}
+        },
+        linger: {
+          '0%, 100%': {opacity: 0},
+          '30%, 70%': {opacity: 0.25}
+        },
+        pulse: {
+          '0%, 100%': {transform: "scale(0.1)"},
+          '40%, 60%': {transform: "scale(1)"}
         }
       },
       animation: {
         'seesaw': 'seesaw 1s ease-in-out infinite',
+        'twinkle': 'twinkle 1s ease-in-out 1',
+        'linger': 'linger 7s ease-in-out 1',
+        'pulse': 'pulse 7s ease-in-out 1'
       }
     },
   },
