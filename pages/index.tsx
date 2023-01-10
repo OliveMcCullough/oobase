725935d9749a474ffd9769d3645e91ad11ce94c9
Olive McCullough <olive.m.mccullough@gmail.com>
2023-01-10 00:21:00 +0000

    Changes to ooblets display page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,29 +36,44 @@ export default function Home({
 }] 
 }) {
   return (
-    <div>
-      <div> </div>
-      <div className="ooblets-style container">
-        <h1 className="ooblets-title"> Welcome to Oobase! </h1>
-        <h2 className="ooblets-subtitle"> These ooblets can be found in Oob:</h2>
-        { ooblets.length > 0 && 
-        <ul className="ooblet-list"> 
-          {ooblets.map((ooblet) => (
-            <li key={ooblet.id} className="ooblet-card">
-              <a>
-                <div className="ooblet-card-display-container"> <img className="ooblet-card-display-image" alt={ooblet.name} src={`/images/ooblets/${ooblet.name}_common.png`}/>  </div>
-                <span className="ooblet-tag common-ooblet-name-tag"> {ooblet.name}  </span>
-              </a>
-              {ooblet.regions.length > 0 &&
+    <Layout>
+      <div>
+        <div className="ooblets-style container">
+          <div>
+            <h1 className="ooblets-title"> Welcome to Oobase! </h1>
+            <h2 className="ooblets-subtitle"> These ooblets can be found in Oob:</h2>
+          </div>
+          { ooblets.length > 0 && 
+          <ul className="ooblet-list"> 
+            {ooblets.map((ooblet) => (
+              <li key={ooblet.id} className="ooblet-card">
                 <a>
-                  {ooblet.regions.map((region) => (
-                    <span key={region.id} className="ooblet-tag"> {region.name} </span>
-                  ))}
+                  <div className="ooblet-card-display-container">
+                    <img 
+                      className="ooblet-card-display-image" 
+                      alt={ooblet.name} 
+                      src={`/images/ooblets/${ooblet.name}_common.png`}
+                      onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        console.log("hi");
+                        currentTarget.src="/images/ooblets/Dunno.png";
+                      }}
+                    />  
+                  </div>
+                  <span className="ooblet-tag common-ooblet-name-tag"> {ooblet.name}  </span>
                 </a>
-              }
-            </li>))} 
-        </ul>}
+                {ooblet.regions.length > 0 &&
+                  <>{ooblet.regions.map((region) => (
+                    <a key={region.id}>
+                      <span className="ooblet-tag"> {region.name} </span>
+                    </a>
+                  ))}
+                  </>
+                }
+              </li>))} 
+          </ul>}
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
