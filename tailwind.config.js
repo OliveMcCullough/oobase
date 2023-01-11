f8dfc860b43f2f804aa15596bf885de6f23913be
Olive McCullough <olive.m.mccullough@gmail.com>
2023-01-11 14:58:24 +0000

    CSS updates and tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,24 @@ module.exports = {
       fontFamily: {
         'franxurter-totally': ['Franxurter Totally'],
         'franxurter-totally-fat': ['Franxurter Totally Fat'],
+        'multicolore': ['Multicolore'],
       },
       keyframes: {
-        seesaw: {
+        seesaw_large: {
           '0%, 100%': {transform: 'rotate(5deg)'},
           '50%': {transform: 'rotate(-5deg)'}
+        },
+        seesaw_large_reverse: {
+          '0%, 100%': {transform: 'rotate(-5deg)'},
+          '50%': {transform: 'rotate(5deg)'}
+        },
+        seesaw_medium: {
+          '0%, 100%': {transform: 'rotate(1.5deg)'},
+          '50%': {transform: 'rotate(-1.5deg)'}
+        },
+        seesaw_small: {
+          '0%, 100%': {transform: 'rotate(0.5deg)'},
+          '50%': {transform: 'rotate(-0.5deg)'}
         },
         twinkle: {
           '0%, 100%': {opacity: 0},
@@ -29,7 +42,10 @@ module.exports = {
         }
       },
       animation: {
-        'seesaw': 'seesaw 1s ease-in-out infinite',
+        'seesaw-large': 'seesaw_large 1s ease-in-out infinite',
+        'seesaw-reverse-large-fast': 'seesaw_large_reverse 0.75s ease-in-out infinite',
+        'seesaw-medium-fast': 'seesaw_medium 0.75s ease-in-out infinite',
+        'seesaw-small-fast': 'seesaw_small 0.75s ease-in-out infinite',
         'twinkle': 'twinkle 1s ease-in-out 1',
         'linger': 'linger 7s ease-in-out 1',
         'pulse': 'pulse 7s ease-in-out 1'
